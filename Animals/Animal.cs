35724f2d1312db8a5b64f5110723a638382b1996
Stephen Szpak <srszpak@gmail.com>
2017-01-14 16:31:02 -0600

    add 3 new animals and consoled them out

---
+++ Animals/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Zoolandia.Animals
+{
+    class Animal
+    {
+        public string name { get; set; }
+        public string size { get; set; }
+        public string color { get; set; }
+        public string UniqueAttribute { get; set; }
+        
+    }
+}
